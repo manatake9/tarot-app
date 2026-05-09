037563d8578ad912da65a0662860f579750fcc42
manatake9 <manato0122@outlook.jp>
2026-05-09 19:38:58 +0900

    意味を加えた

--- src/lib/tarot.ts
+++ src/lib/tarot.ts
@@ -3,21 +3,62 @@ export const cards = [
     id: 0,
     name: "The Fool",
     image: "/cards/THE FOOL.png",
-    meaningUp: "新しい始まり",
-    meaningRev: "無計画"
+
+    symbolism: [
+      "崖の端に立つ若者","白い犬","小さな荷物","太陽","白い花","ワンド","0",
+    ],
+    upright: {
+      keywords: [
+        "始まり","自由","可能性","冒険","無邪気",
+      ],
+    },
+    reversed: {
+      keywords: [
+        "無計画","軽率","現実逃避","怠慢","未熟","愚か"
+      ],
+    }
   },
+
   {
-    id: 1,
-    name: "The Magician",
-    image: "/cards/THE MAGICIAN.png",
-    meaningUp: "創造力",
-    meaningRev: "空回り"
+  id: 1,
+  name: "The Magician",
+  image: "/cards/THE MAGICIAN.png",
+
+  symbolism: [
+    "テーブルの上の道具","上を指す右手","下を指す左手","無限大の記号","赤いローブ","薔薇","百合","1"
+  ],
+  upright: {
+    keywords: [
+      "創造","才能","実現","行動力","自信","集中","アイデア","閃き"
+    ],
   },
-  {
-    id: 2,
-    name: "The High Priestess",
-    image: "/cards/THE HIGH PRIESTESS.png",
-    meaningUp: "内面の知恵",
-    meaningRev: "無知"
+
+  reversed: {
+    keywords: [
+      "自信がない","虚勢","不誠実","操作","迷い","不信","不名誉"
+    ],
   }
+},
+
+{
+  id: 2,
+  name: "The High Priestess",
+  image: "/cards/THE HIGH PRIESTESS.png",
+
+  symbolism: [
+    "黒と白の柱","巻物","月の冠","青いローブ","ベール","ザクロ模様","三日月","水","2"
+  ],
+
+  upright: {
+    keywords: [
+      "直感","知恵","神秘","静寂","洞察","内面","潜在意識","バランス"
+    ],
+  },
+
+  reversed: {
+    keywords: [
+      "無知","混乱","不安","秘密","神経質","疑念","冷酷"
+    ],
+  }
+}
 ]
